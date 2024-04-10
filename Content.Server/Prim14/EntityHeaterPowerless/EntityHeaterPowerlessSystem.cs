7f7ca68e72d6a14dec47ea014cb6d1d202b161c1
HoofedEar <HoofedEar@users.noreply.github.com>
2024-04-10 14:58:49 -0700

    contstructable bonfire, that can cook things, and turns to ash after 3 mins

---
+++ Content.Server/Prim14/EntityHeaterPowerless/EntityHeaterPowerlessSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Temperature.Systems;
+using Content.Shared.Placeable;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server.Prim14.EntityHeaterPowerless;
+
+public sealed class EntityHeaterPowerlessSystem: EntitySystem
+{
+    [Dependency] private readonly TemperatureSystem _temperature = default!;
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<EntityHeaterPowerlessComponent, StartCollideEvent>(OnStartCollide);
+    }
+
+    public override void Update(float deltaTime)
+    {
+        var query = EntityQueryEnumerator<EntityHeaterPowerlessComponent, ItemPlacerComponent>();
+        while (query.MoveNext(out var uid, out var comp, out var placer))
+        {
+            // don't divide by total entities since its a big grill
+            // excess would just be wasted in the air but that's not worth simulating
+            // if you want a heater thermomachine just use that...
+            var energy = 30.0f;
+            foreach (var ent in placer.PlacedEntities)
+            {
+                _temperature.ChangeHeat(ent, energy);
+            }
+        }
+    }
+
+    private void OnStartCollide(EntityUid uid, EntityHeaterPowerlessComponent comp, ref StartCollideEvent args)
+    {
+        var otherUid = args.OtherEntity;
+
+        if (TryComp<FlammableComponent>(otherUid, out var flammable))
+        {
+            // Apply the fury of a thousand suns
+            var multiplier = flammable.FireStacks == 0f ? 5f : 1f;
+            _flammable.AdjustFireStacks(otherUid, 0.50f * multiplier, flammable);
+            _flammable.Ignite(otherUid, uid, flammable);
+        }
+    }
+}
---
+++ Content.Server/Tiles/WaterTileSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Temperature.Components;
+using Content.Server.Temperature.Systems;
+using Content.Shared.StepTrigger.Systems;
+
+namespace Content.Server.Tiles;
+
+public sealed class WaterTileSystem : EntitySystem
+{
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+    [Dependency] private readonly TemperatureSystem _temperature = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<WaterTileComponent, StepTriggeredOffEvent>(OnWaterStepTriggered);
+        SubscribeLocalEvent<WaterTileComponent, StepTriggerAttemptEvent>(OnWaterStepTriggerAttempt);
+    }
+
+    private void OnWaterStepTriggerAttempt(EntityUid uid, WaterTileComponent component, ref StepTriggerAttemptEvent args)
+    {
+        if (!HasComp<FlammableComponent>(args.Tripper))
+            return;
+
+        args.Continue = true;
+    }
+
+    private void OnWaterStepTriggered(EntityUid uid, WaterTileComponent component, ref StepTriggeredOffEvent args)
+    {
+        var otherUid = args.Tripper;
+
+        if (TryComp<FlammableComponent>(otherUid, out var flammable))
+        {
+            // Extinguish them if they're on fire
+            _flammable.Extinguish(otherUid, flammable);
+        }
+
+        if (TryComp<TemperatureComponent>(otherUid, out var temperature))
+        {
+            // Cool them down
+            _temperature.ChangeHeat(otherUid, component.CoolingRate, true, temperature);
+        }
+    }
+}
