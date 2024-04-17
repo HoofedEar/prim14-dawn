75469172ca1e09cd5489d7726bff1763e5b86cbf
HoofedEar <HoofedEar@users.noreply.github.com>
2024-04-17 14:22:02 -0700

    bushes and berries!

---
+++ Content.Client/Prim14/Bushes/BushVisualsComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Client.Prim14.Bushes;
+
+[RegisterComponent]
+public sealed partial class BushVisualsComponent : Component
+{
+    [DataField("stateReady")]
+    public string? StateReady;
+
+    [DataField("stateEmpty")]
+    public string? StateEmpty;
+}
