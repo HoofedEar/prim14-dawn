492e9bdbf316bf12d24b55c24da5069cae7e3abf
HoofedEar <HoofedEar@users.noreply.github.com>
2024-04-23 13:39:01 -0700

    mini hoe, pestle and mortar

---
+++ Content.Server/Prim14/PestleMortar/PestleMortarComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server.Prim14.PestleMortar;
+
+[RegisterComponent]
+[Access(typeof(PestleMortarSystem))]
+public sealed partial class PestleMortarComponent : Component
+{
+
+}
+
+
+public static class SharedPestleMortar
+{
+    public static readonly string InputContainerId = "inputContainer";
+}
